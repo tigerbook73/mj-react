887307ad4c95a1e3f7dab1792d30908fa95b093d
Shengtian Liao <tigerbook73@outlook.com>
2025-05-11 22:37:10 +1000

    feat: refactor SignInPage to use react-hook-form with Zod validation; remove manual form handling and improve input management

--- src/app/SignInPage.tsx
+++ src/app/SignInPage.tsx
@@ -1,27 +1,42 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 import useMJStore from "@/stores/mj-store";
 import { clientApi } from "@/client/client-api";
 import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+const formSchema = z
+  .object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(3, "Password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password !== data.email, {
+    message: "Password cannot be the same as email",
+    path: ["password"],
+  });
 
 export default function SignInPage() {
   const user = useMJStore((state) => state.user);
   const setUser = useMJStore((state) => state.setUser);
   const setSignedIn = useMJStore((state) => state.setSignedIn);
 
-  const [formData, setFormData] = useState({ email: user.email, password: user.password });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: user.email,
+      password: user.password,
+    },
+    mode: "onBlur",
+  });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const onSubmit = async (formData: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
       await clientApi.signIn(formData.email, formData.password);
@@ -35,44 +50,45 @@ export default function SignInPage() {
     }
   };
 
-  useEffect(() => {
-    if (user.email && user.password) {
-      setFormData({ email: user.email, password: user.password });
-    }
-  }, [user.email, user.password]);
-
   return (
     <div className="flex justify-center items-center w-full">
-      <form onSubmit={handleSubmit} className="min-w-100 w-1/3 space-y-4 p-6 border rounded-md shadow-md">
-        <div>
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="min-w-100 w-1/3 space-y-4 p-6 border rounded-md shadow-md"
+        >
+          <FormField
+            control={form.control}
             name="email"
-            type="email"
-            placeholder="Enter your email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div>
-          <Label htmlFor="password">Password</Label>
-          <Input
-            id="password"
+          <FormField
+            control={form.control}
             name="password"
-            type="password"
-            placeholder="Enter your password"
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-1"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <Button type="submit" className="w-full" disabled={loading}>
-          {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          {loading ? "Sign In ..." : "Sign In"}
-        </Button>
-      </form>
+          <Button type="submit" className="w-full" disabled={loading}>
+            {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            {loading ? "Sign In ..." : "Sign In"}
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }

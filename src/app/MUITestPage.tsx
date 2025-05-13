faaaa01437b1204b218af2fa83dcf60e3ca86bf5
Shengtian Liao <tigerbook73@outlook.com>
2025-05-13 23:54:47 +1000

    feat: wrap buttons in ScopedCssBaseline for improved theming support

--- src/app/MUITestPage.tsx
+++ src/app/MUITestPage.tsx
@@ -1,17 +1,19 @@
-import { Button as MUIButton } from "@mui/material";
+import { Button as MUIButton, ScopedCssBaseline } from "@mui/material";
 
 export default function MUITestPage() {
   return (
-    <div className="flex gap-2">
-      <MUIButton variant="contained" color="primary">
-        Primary
-      </MUIButton>
-      <MUIButton variant="outlined" color="secondary">
-        Secondary
-      </MUIButton>
-      <MUIButton variant="text" color="error">
-        Error
-      </MUIButton>
-    </div>
+    <ScopedCssBaseline enableColorScheme>
+      <div className="flex gap-2">
+        <MUIButton variant="contained" color="primary">
+          Primary
+        </MUIButton>
+        <MUIButton variant="outlined" color="secondary">
+          Secondary
+        </MUIButton>
+        <MUIButton variant="text" color="error">
+          Error
+        </MUIButton>
+      </div>
+    </ScopedCssBaseline>
   );
 }

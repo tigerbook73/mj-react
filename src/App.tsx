46031d40659aa65d8621c96cb519766341f00255
Shengtian Liao <tigerbook73@outlook.com>
2025-05-13 23:54:39 +1000

    refactor: lazy load components in App for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./app/Home";
-import ConnectingPage from "./app/ConnectingPage";
-import LobbyPage from "./app/LobbyPage";
-import GamePage from "./app/GamePage";
 import MainLayout from "./app/MainLayout";
-import SignInPage from "./app/SignInPage";
-import NotFound from "./app/NotFound";
 import { Toaster } from "./components/ui/sonner";
-import TestPage from "./app/TestPage";
-import MUITestPage from "./app/MUITestPage";
+import React, { Suspense } from "react";
 
 export function App() {
+  const ConnectingPage = React.lazy(() => import("./app/ConnectingPage"));
+  const LobbyPage = React.lazy(() => import("./app/LobbyPage"));
+  const GamePage = React.lazy(() => import("./app/GamePage"));
+  const SignInPage = React.lazy(() => import("./app/SignInPage"));
+  const NotFound = React.lazy(() => import("./app/NotFound"));
+  const TestPage = React.lazy(() => import("./app/TestPage"));
+  const MUITestPage = React.lazy(() => import("./app/MUITestPage"));
+  const Home = React.lazy(() => import("./app/Home"));
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/connecting" element={<ConnectingPage />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/lobby" element={<LobbyPage />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/test/mui" element={<MUITestPage />} />
-          <Route path="/*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div className="flex justify-center items-center h-screen">Loading...</div>}>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/connecting" element={<ConnectingPage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/lobby" element={<LobbyPage />} />
+            <Route path="/game" element={<GamePage />} />
+            <Route path="/test" element={<TestPage />} />
+            <Route path="/test/mui" element={<MUITestPage />} />
+            <Route path="/*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
       <Toaster position="top-center" />
     </BrowserRouter>
   );
